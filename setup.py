ea2948b85d7e4c3620758d629a529a26f203e7d2
Ofer Koren <koreno@gmail.com>
2017-06-08 16:03:37 +0300

    V0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='weka-easypy',
-      version='0.1.0',
+      version='0.1.1',
       description='easypy is a collection of python modules that makes developers happy',
       author='Ofer Koren',
       author_email='koreno [at] gmail.com',
