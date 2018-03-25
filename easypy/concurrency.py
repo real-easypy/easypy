9a8a0a8695c60dd2cef120fb87670d237dc1f405
Ofer Koren <koreno@gmail.com>
2018-03-26 00:49:13 +0300

    concurrency: added 'zip_with_index'
    instead of MultiObject(enumerate(items)).call(lambda idx, item:...), which loses the log_ctxs
    use items.zip_with_index().call(lambda item, idx:...)

--- easypy/concurrency.py
+++ easypy/concurrency.py
@@ -651,6 +651,15 @@ class MultiObject(object):
         assert len(mo) == len(self), "All collection must have at least %s items" % len(self)
         return mo
 
+    def zip_with_index(self):
+        """
+        Replaces this pattern, which loses the log contexts:
+            >> MultiObject(enumerate(items)).call(lambda idx, item: ...)
+        with this pattern, which retains log contexts:
+            >> MultiObject(items).zip_with_index().call(lambda item, idx: ...)
+        """
+        return self.zip_with(range(len(self)))
+
     def zip(self):
         "Concurrently iterate through the iterables contained by this MultiObject"
         iters = list(map(iter, self))
