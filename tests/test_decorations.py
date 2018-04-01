5563f96e5f095295505d2ff511ed5e3ac3f74e49
Idan Arye <idan@weka.io>
2018-04-01 12:29:29 +0300

    Add `lazy_decorator`

--- tests/test_decorations.py
+++ tests/test_decorations.py
@@ -1,6 +1,8 @@
 import pytest
 
-from easypy.decorations import deprecated_arguments, kwargs_resilient
+from functools import wraps
+
+from easypy.decorations import deprecated_arguments, kwargs_resilient, lazy_decorator
 
 
 def test_deprecated_arguments():
@@ -47,3 +49,65 @@ def test_kwargs_resilient():
     # Should be passing c because it's not in `negligible`
     # Should not be passing d because it's in `negligible` and not in the function signature
     assert qux(1, b=2, c=3, d=4) == [1, 2, {'c': 3}]
+
+
+def test_lazy_decorator_lambda():
+    def add_to_result(num):
+        def inner(func):
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                return func(*args, **kwargs) + num
+
+            wrapper.__name__ = '%s + %s' % (func.__name__, num)
+
+            return wrapper
+        return inner
+
+    class Foo:
+        def __init__(self, num):
+            self.num = num
+
+        @lazy_decorator(lambda self: add_to_result(num=self.num))
+        def foo(self):
+            """foo doc"""
+            return 1
+
+    foo = Foo(10)
+    assert foo.foo() == 11
+
+    assert Foo.foo.__name__ == 'foo'
+    assert foo.foo.__name__ == 'foo + 10'
+
+    assert Foo.foo.__doc__ == foo.foo.__doc__ == 'foo doc'
+
+
+def test_lazy_decorator_attribute():
+    class Foo:
+        def add_to_result(self, func):
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                return func(*args, **kwargs) + self.num
+
+            wrapper.__name__ = '%s + %s' % (func.__name__, self.num)
+
+            return wrapper
+
+        @lazy_decorator('add_to_result')
+        def foo(self):
+            """foo doc"""
+            return 1
+
+    foo = Foo()
+
+    with pytest.raises(AttributeError):
+        # We did not set foo.num yet, so the decorator will fail trying to set the name
+        foo.foo
+
+    foo.num = 10
+    assert foo.foo() == 11
+    assert foo.foo.__name__ == 'foo + 10'
+    assert Foo.foo.__doc__ == foo.foo.__doc__ == 'foo doc'
+
+    foo.num = 20
+    assert foo.foo() == 21
+    assert foo.foo.__name__ == 'foo + 20'
