7bd0955d43a9e6efa9ddba173888808a4a1154cc
Ofer Koren <koreno@gmail.com>
2019-06-10 17:25:06 +0300

    setup: updated to new pypi standards

--- setup.py
+++ setup.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import setuptools
 
-setup(name='weka-easypy',
-      version='0.3.0',
-      description='easypy is a collection of python modules that makes developers happy',
-      author='Ofer Koren',
-      author_email='koreno [at] gmail.com',
-      url='https://github.com/weka-io/easypy',
-      license='BSD',
-      packages=['easypy'])
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='weka-easypy',
+    version='0.3.0',
+    description='easypy is a collection of python modules that makes developers happy',
+    author='Ofer Koren',
+    author_email='koreno@gmail.com',
+    url='https://github.com/weka-io/easypy',
+    license='BSD',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+)
+
 
 # how to upload a package:
 # 0. increment the version above
-# 1. python setup.py register
-# 2. python setup.py sdist upload
+# 1. python3 setup.py sdist bdist_wheel
+# 2. python3 -m twine upload dist/*
