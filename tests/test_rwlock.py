0ebbba716503fdb3180fe2d689e949f64ef6bb34
Ofer Koren <koreno@gmail.com>
2020-06-22 09:09:01 +0300

    sync: allow specifying identity of reader when using RWLock

--- tests/test_rwlock.py
+++ tests/test_rwlock.py
@@ -1,5 +1,5 @@
 import threading
-
+import pytest
 import logging
 from easypy.concurrency import concurrent
 from easypy.sync import RWLock
@@ -76,3 +76,32 @@ def test_rwlock():
         main_ctrl.clear()
         logging.info("write lock released")
         assert not state.reading and not state.writing
+
+
+def test_rwlock_different_threads():
+    lock = RWLock("test")
+    ea = threading.Event()
+    eb = threading.Event()
+
+    def a(id=None):
+        lock.acquire(id)
+        ea.set()
+        eb.wait()
+
+    def b(id=None):
+        lock.release(id)
+        eb.set()
+
+    with concurrent(a):
+        ea.wait()
+        assert lock.owners
+        with pytest.raises(RuntimeError):
+            b()
+        eb.set()
+        assert lock.owners
+
+    with concurrent(a, "same"):
+        assert lock.owners
+        with concurrent(b, "same"):
+            pass
+        assert lock.owners
